1bfbfd26714ae22b3221eeb5382079dce0c29ffa
Tristan Pacba <tpacba@live.com>
2020-09-26 18:47:17 -0700

    set random coordinates for snake-food

--- snakeport/src/components/GameArea/index.js
+++ snakeport/src/components/GameArea/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import Snake from './Snake';
+import Food from './Food';
+
+function getRandomCoordinates() {
+    const min = 0;
+    const max = 96;
+
+    let x = Math.floor(Math.random() * (max - min) + min);
+    let y = Math.floor(Math.random() * (max - min) + min);
+    return [x, y]
+}
 
 class GameArea extends React.Component {
+    state = {
+        food: getRandomCoordinates(),
+        snakeDots: [
+            [0, 0],
+            [4, 0],
+            [8, 0],
+            [12, 0]
+        ]
+    }
+
     render() {
         return (
-            <div className="game-area"></div>
+            <div className="game-area">
+                <Snake snakeDots={this.state.snakeDots}></Snake>
+                <Food dot={this.state.food}></Food>
+            </div>
         );
     }
 }
