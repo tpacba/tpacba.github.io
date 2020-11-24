cd1442ce54c933bc57fb993bafc5e0cc1d7a72e0
Tristan Pacba <tpacba@live.com>
2020-11-24 09:08:55 -0800

    Updates

---
+++ precache-manifest.da13efd92ca732c79c825c2b077446da.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cda59100a03b6b065dca05bdee1c2a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4d9fe067da28037e68d",
+    "url": "/static/css/main.a607d3d1.chunk.css"
+  },
+  {
+    "revision": "a605cff13ddc48806e3a",
+    "url": "/static/js/2.34ad6d7b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.34ad6d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d9fe067da28037e68d",
+    "url": "/static/js/main.6a37e160.chunk.js"
+  },
+  {
+    "revision": "dfd015be73cc892d231d",
+    "url": "/static/js/runtime-main.b0382e83.js"
+  },
+  {
+    "revision": "9dacab9cb67603abea48d240512dc669",
+    "url": "/static/media/cowabunga-demo.9dacab9c.png"
+  },
+  {
+    "revision": "133f1154fed0e648e2ab6b608cbeb9dc",
+    "url": "/static/media/directory.133f1154.png"
+  },
+  {
+    "revision": "15c21fb504c0bc814ea620ffbcc05c9a",
+    "url": "/static/media/fitnesstracker.15c21fb5.png"
+  },
+  {
+    "revision": "ddaa70335a649672ed2c43982901990b",
+    "url": "/static/media/instagraph-ql.ddaa7033.png"
+  },
+  {
+    "revision": "8454e338fcac9966ce30a513b758437d",
+    "url": "/static/media/profile.8454e338.jpg"
+  },
+  {
+    "revision": "361f78127bee51395595845d9d7b69ba",
+    "url": "/static/media/smood.361f7812.png"
+  },
+  {
+    "revision": "e2115b79fa6c33da4a4670c888c57193",
+    "url": "/static/media/snakport.e2115b79.png"
+  },
+  {
+    "revision": "5a9e8ccf0c8d24ba1c805325e98f829b",
+    "url": "/static/media/tinderforrestaurants.5a9e8ccf.png"
+  },
+  {
+    "revision": "6ee6bf8c7adc244dff422b1b224c164a",
+    "url": "/static/media/wallpaper.6ee6bf8c.jpeg"
+  }
+]);
